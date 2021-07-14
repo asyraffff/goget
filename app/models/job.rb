a347ad6ebd730a656df459bb026308128a0b75ab
asyraffff <amirulasyraf4.0x@gmail.com>
2021-07-14 12:01:29 +0800

    Add Job's model settings

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ApplicationRecord
-  belongs_to :user
+  default_scope { order(created_at: :desc) }
+	belongs_to :user
+	validates :pickup_address, :dropoff_address, :pickup_lat, :pickup_lng, :dropoff_lat, :dropoff_lng, presence: true
 end
