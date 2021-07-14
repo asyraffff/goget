0a6820f240778d4563bf75e3d62c606b25019130
asyraffff <amirulasyraf4.0x@gmail.com>
2021-07-14 10:20:02 +0800

    Fix: file cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'render/index'
 
+  root 'render#index'
 end
--- db/migrate/20210714012622_add_key_in_user.rb
+++
@@ -1,5 +0,0 @@
-class AddKeyInUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :is_gogetter, :boolean
-  end
-end
